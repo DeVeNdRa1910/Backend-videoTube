8e25029992b430b531f3a372285edb90f97336a5
Devendra <devendrav1910@gmail.com>
2024-06-06 16:33:55 +0530

    connect to Mongo atlas

---
+++ src/index.js
@@ -0,0 +1,46 @@
+// require('dotenv').config({path: './env'})
+//OR
+import dotenv from 'dotenv';
+
+
+//dotenv is use to As early possible in your application, import and configure dotenv.
+
+dotenv.config({
+  path: './env'
+});
+
+//There two to connect backend with MongoDB
+//m-1
+import connectDB from "./db/index.js";
+
+connectDB();
+
+
+
+
+
+
+//m-2
+
+/* 
+import mongoose from "mongoose";
+import { DB_NAME } from "./constants";
+import express from "express";
+
+const app = express();
+( async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+    app.on("error" , (error)=>{
+      console.error("Error: ", error);
+      throw error;
+    })
+
+    app.listen(process.env.PORT , ()=>{
+      console.log(`App running on port ${process.env.PORT}`);
+    })
+  } catch (error) {
+    console.error("Error: ", error);
+    throw error;
+  }
+})() */
